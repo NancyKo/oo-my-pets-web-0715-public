cb87c901cdb4ebfb985e3ab7a9036a2e4ea92478
N K <konancys@gmail.com>
2015-08-08 08:56:32 -0400

    Almost done with owner, pushing what's done so far

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
   # code goes here
+ attr_reader :type, :species
+ attr_accessor :name
+  def initialize(type)
+  	@type = type
+  	@species = "human"	
+    @pets = {}
+  end
+
+  def say_species
+  	"I am a #{@species}."
+  end
+
+  def name=(name)
+  	@name = name
+  end
 end
