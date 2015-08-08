978c6a0698ccb6849c9ce2ce6781587465ff71b5
N K <konancys@gmail.com>
2015-08-08 15:48:19 -0400

    Adding the rest of instance methods for owner

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,11 @@ require 'pry'
 class Owner
   # code goes here
  attr_reader :type, :species
- attr_accessor :name, :pets
+ attr_accessor :name, :pets, :mood
 
+  def self.all
+  end
+  
   def initialize(type)
   	@type = type
   	@species = "human"	
@@ -25,16 +28,40 @@ class Owner
     @pets 
   end
 
-  def buy_fish(fish_name)
-    self.pets[:fishes] << fish_name
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
   end
 
-  def buy_cat(cat_name)
-    self.pets[:cats] << cat_name
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
   end
 
-  def buy_dog(dog_name)
-    self.pets[:dogs] << dog_name
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
   end
 
+  def walk_dogs
+   pets[:dogs].each { |d| d.mood = "happy" }
+  end
+
+  def play_with_cats
+    pets[:cats].each { |c| c.mood = "happy" }
+  end
+
+  def feed_fish
+    pets[:fishes].each { |f| f.mood = "happy" }
+  end
+
+  def sell_pets    
+    @pets.each do |pet_type_key, pet_array|
+      pet_array.each do |individual_pet|
+        individual_pet.mood = "nervous"
+        pets.delete(pet_type_key)
+      end
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
 end
